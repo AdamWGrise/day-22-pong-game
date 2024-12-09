7ca8570cfda8ace197ddd68210765277336a3d56
Adam Grise <AdamWGrise@gmail.com>
2024-12-08 19:02:44 -0600

    Refactored bounce logic, added paddle bounce logic

--- ball.py
+++ ball.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+import random
+
 
 class Ball(Turtle):
 
@@ -8,8 +10,31 @@ class Ball(Turtle):
         self.goto(position)
         self.color("white")
         self.penup()
+        self.v_distance = 10
+        self.h_distance = 10
+
+    def move_up(self, distance):
+        return self.ycor() + distance
+
+    def move_down(self, distance):
+        return self.ycor() - distance
 
     def move(self):
-        new_x = self.xcor() + 1
-        new_y = self.ycor() + 1
+        new_x = self.xcor() + self.h_distance
+        new_y = self.ycor() + self.v_distance
         self.goto(new_x, new_y)
+
+    def wall_bounce(self):
+        self.v_distance *= -1
+
+    def paddle_bounce(self):
+        self.h_distance *= -1
+        # if self.h_distance < 0:
+        #     self.h_distance -= 0.05 * random.randint(3, 10)
+        # else:
+        #     self.h_distance += 0.05 * random.randint(3, 10)
+
+    def point_score(self, side):
+        print("Point for " + side + " player")
+        self.goto(0, 0)
+        self.h_distance *= -1
--- main.py
+++ main.py
@@ -22,8 +22,22 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.016)
+    time.sleep(0.05)
     screen.update()
     ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.wall_bounce()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        print("Hit at " + str(ball.xcor()) + ", " + str(ball.ycor()))
+        ball.paddle_bounce()
+
+    if ball.xcor() > 380:
+        ball.point_score("left")
+
+    if ball.xcor() < -380:
+        ball.point_score("right")
+
 
 screen.exitonclick()
